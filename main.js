c5522b3344fe6ba91409c5266535f82b95adb15c
Gachiman <aglikov@bk.ru>
2023-03-28 16:09:09 +0300

    Add disabled buttons (need refactoring)

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ class ArrangeBoxContainer {
       this.initialAvailableValues.push(this.generateRandomString());
     }
 
-    this.clickedListItem = undefined;
+    this.clickedListItem = null;
+    this.createdButtonsBox = [];
 
     this.listAvailable = this.createList(this.initialAvailableValues);
     this.listSelected = this.createList();
@@ -29,11 +30,12 @@ class ArrangeBoxContainer {
       ['\u25B2', '\u25B3', '\u25BD', '\u25BC', '\u271A'],
       [
         () => this.upByAll(this.listAvailable),
-        () => this.upByOne(),
-        () => this.downByOne(),
+        () => this.upByOne(this.listAvailable),
+        () => this.downByOne(this.listAvailable),
         () => this.downByAll(this.listAvailable),
         () => this.addListElement(this.listAvailable)
-      ]
+      ],
+      [true, true, true, true, false]
     );
 
     let leftBoxArea = this.createBoxArea('Available');
@@ -46,7 +48,8 @@ class ArrangeBoxContainer {
         () => this.moveOneToSelected(),
         () => this.moveOneToAvailable(),
         () => this.moveAllToAvailable(),
-      ]
+      ],
+      [false, true, true, true]
     );
 
     let rightBoxArea = this.createBoxArea('Selected');
@@ -56,11 +59,12 @@ class ArrangeBoxContainer {
       ['\u25B2', '\u25B3', '\u25BD', '\u25BC', '\u271A'],
       [
         () => this.upByAll(this.listSelected),
-        () => this.upByOne(),
-        () => this.downByOne(),
+        () => this.upByOne(this.listSelected),
+        () => this.downByOne(this.listSelected),
         () => this.downByAll(this.listSelected),
         () => this.addListElement(this.listSelected)
-      ]
+      ],
+      [true, true, true, true, false]
     );
 
     arrangeBoxArea.append(leftButtonsArea, leftBoxArea, middleButtonsArea, rightBoxArea, rightButtonsArea);
@@ -69,7 +73,7 @@ class ArrangeBoxContainer {
   }
 
   // create buttons for arrangeBoxArea
-  createButtons(amount, opSymbols, operations) {
+  createButtons(amount, opSymbols, operations, isDisabled) {
     let buttonsContainer = document.createElement('div');
     buttonsContainer.className = 'arrange_box_area_buttons';
 
@@ -77,7 +81,12 @@ class ArrangeBoxContainer {
       let button = document.createElement('button');
       button.innerText = opSymbols[i];
       button.addEventListener('click', operations[i]);
+      if (isDisabled[i]) {
+        this.disableButton(button);
+      }
+
       buttonsContainer.append(button);
+      this.createdButtonsBox.push(button);
     }
 
     return buttonsContainer;
@@ -139,10 +148,12 @@ class ArrangeBoxContainer {
   }
 
   clickListItem(clickedListItem) {
+    this.clickListItemDisableButton(clickedListItem);
+
     if (this.clickedListItem) {
       this.clickedListItem.classList.remove('clicked_list_item');
       if (this.clickedListItem === clickedListItem) {
-        this.clickedListItem = undefined;
+        this.clickedListItem = null;
       } else {
         this.clickedListItem = clickedListItem;
         this.clickedListItem.classList.add('clicked_list_item');
@@ -153,28 +164,133 @@ class ArrangeBoxContainer {
     }
   }
 
+  clickListItemDisableButton(clickedListItem) {
+    let wasActiveItem = !!this.clickedListItem;
+
+    let sortButtons = [];
+    if (clickedListItem.parentElement == this.listAvailable) {
+      for (let i = 0; i < 4; i++) {
+        sortButtons.push(this.createdButtonsBox[i]);
+      }
+      this.undisableButton(this.createdButtonsBox[6]);
+      this.disableButton(this.createdButtonsBox[7]);
+    } else {
+      for (let i = 0; i < 4; i++) {
+        sortButtons.push(this.createdButtonsBox[i + 9]);
+      }
+      this.undisableButton(this.createdButtonsBox[7]);
+      this.disableButton(this.createdButtonsBox[6]);
+    }
+
+    if (wasActiveItem) {
+      if (this.clickedListItem === clickedListItem) {
+        for (let button of sortButtons) {
+          this.disableButton(button);
+        }
+        this.disableButton(this.createdButtonsBox[6], this.createdButtonsBox[7]);
+      } else {
+        if (clickedListItem.previousElementSibling) {
+          this.undisableButton(sortButtons[0], sortButtons[1]);
+        } else {
+          this.disableButton(sortButtons[0], sortButtons[1]);
+        }
+        if (clickedListItem.nextElementSibling) {
+          this.undisableButton(sortButtons[2], sortButtons[3]);
+        } else {
+          this.disableButton(sortButtons[2], sortButtons[3]);
+        }
+      }
+    } else {  
+      if (clickedListItem.previousElementSibling) {
+        this.undisableButton(sortButtons[0], sortButtons[1]);
+      }
+      if (clickedListItem.nextElementSibling) {
+        this.undisableButton(sortButtons[2], sortButtons[3]);
+      }
+    }
+  }
+
+  disableMoveButton(targetedList) {
+    let sortButtons = [];
+    if (targetedList === this.listAvailable) {
+      for (let i = 0; i < 4; i++) {
+        sortButtons.push(this.createdButtonsBox[i]);
+      }
+    } else {
+      for (let i = 0; i < 4; i++) {
+        sortButtons.push(this.createdButtonsBox[i + 9]);
+      }
+    }
+
+    if (!this.clickedListItem.previousElementSibling) {
+      if (targetedList === this.listAvailable) {
+
+      }
+    }
+  }
+
+  disableButton(...targetButtons) {
+    for (let button of targetButtons) {
+      button.disabled = true;
+      button.classList.add('disabled_button');
+    }
+  }
+
+  undisableButton(...targetButtons) {
+    for (let button of targetButtons) {
+      button.disabled = false;
+      button.classList.remove('disabled_button');  
+    }
+  }
+
+  
   // BUTTONS OPERATIONS /////////////////////////////////
 
   upByAll(targetedList) {
     if (this.clickedListItem) {
       targetedList.firstElementChild.before(this.clickedListItem);
     }
+
+    if (targetedList === this.listAvailable) {
+      this.disableButton(this.createdButtonsBox[0], this.createdButtonsBox[1]);
+      this.undisableButton(this.createdButtonsBox[2], this.createdButtonsBox[3]);
+    } else {
+      this.disableButton(this.createdButtonsBox[9], this.createdButtonsBox[10]);
+      this.undisableButton(this.createdButtonsBox[11], this.createdButtonsBox[12]);
+    }
   }
 
-  upByOne() {
-    if (this.clickedListItem) {
-      let upperSibling = this.clickedListItem.previousElementSibling;
-      if (upperSibling) {
-        upperSibling.before(this.clickedListItem);
+  upByOne(targetedList) {
+    let upperSibling = this.clickedListItem.previousElementSibling;
+    if (upperSibling) {
+      upperSibling.before(this.clickedListItem);
+    }
+    
+    this.undisableButton(this.createdButtonsBox[2], this.createdButtonsBox[3]);
+    this.undisableButton(this.createdButtonsBox[11], this.createdButtonsBox[12]);
+    if (!this.clickedListItem.previousElementSibling) {
+      if (targetedList === this.listAvailable) {
+        this.disableButton(this.createdButtonsBox[0], this.createdButtonsBox[1]);
+      } else {
+        this.disableButton(this.createdButtonsBox[9], this.createdButtonsBox[10]);
       }
     }
   }
 
-  downByOne() {
-    if (this.clickedListItem) {
-      let downerSibling = this.clickedListItem.nextElementSibling;
-      if (downerSibling) {
-        downerSibling.after(this.clickedListItem);
+  downByOne(targetedList) {
+    let downerSibling = this.clickedListItem.nextElementSibling;
+    if (downerSibling) {
+      downerSibling.after(this.clickedListItem);
+    }
+
+    this.undisableButton(this.createdButtonsBox[0], this.createdButtonsBox[1]);
+    this.undisableButton(this.createdButtonsBox[9], this.createdButtonsBox[10]);
+    if (!this.clickedListItem.nextElementSibling) {
+      if (targetedList === this.listAvailable) {
+        this.disableButton(this.createdButtonsBox[2], this.createdButtonsBox[3]);
+        
+      } else {
+        this.disableButton(this.createdButtonsBox[11], this.createdButtonsBox[12]);
       }
     }
   }
@@ -182,6 +298,14 @@ class ArrangeBoxContainer {
   downByAll(targetedList) {
     if (this.clickedListItem) {
       targetedList.lastElementChild.after(this.clickedListItem);
+    }
+
+    if (targetedList === this.listAvailable) {
+      this.disableButton(this.createdButtonsBox[2], this.createdButtonsBox[3]);
+      this.undisableButton(this.createdButtonsBox[0], this.createdButtonsBox[1]);
+    } else {
+      this.disableButton(this.createdButtonsBox[11], this.createdButtonsBox[12]);
+      this.undisableButton(this.createdButtonsBox[9], this.createdButtonsBox[10]);
     }
   }
 
@@ -199,17 +323,50 @@ class ArrangeBoxContainer {
     for (let i = this.listAvailable.children.length - 1; i >= 0; i--) {
       this.listSelected.append(this.listAvailable.children[i]);
     }
+
+    if (this.clickedListItem) {
+      this.clickedListItem.classList.remove('clicked_list_item');
+      this.clickedListItem = null;
+    }
+    this.disableButton(
+      this.createdButtonsBox[0], this.createdButtonsBox[1], this.createdButtonsBox[2],
+      this.createdButtonsBox[3], this.createdButtonsBox[5],
+      this.createdButtonsBox[6], this.createdButtonsBox[7]
+    );
+    this.undisableButton(this.createdButtonsBox[8]);
   }
 
   moveOneToSelected() {
     if (this.clickedListItem) {
       this.listSelected.append(this.clickedListItem);
+
+      this.clickedListItem.classList.remove('clicked_list_item');
+      this.clickedListItem = null;
+      this.disableButton(
+        this.createdButtonsBox[0], this.createdButtonsBox[1], this.createdButtonsBox[2],
+        this.createdButtonsBox[3], this.createdButtonsBox[6], this.createdButtonsBox[7]
+      );
+      this.undisableButton(this.createdButtonsBox[8]);
+      if (!this.listAvailable.children.length) {
+        this.disableButton(this.createdButtonsBox[5]);
+      }
     }
   }
 
   moveOneToAvailable() {
     if (this.clickedListItem) {
       this.listAvailable.append(this.clickedListItem);
+
+      this.clickedListItem.classList.remove('clicked_list_item');
+      this.clickedListItem = null;
+      this.disableButton(
+        this.createdButtonsBox[6], this.createdButtonsBox[7], this.createdButtonsBox[9],
+        this.createdButtonsBox[10], this.createdButtonsBox[11], this.createdButtonsBox[12]
+      );
+      this.undisableButton(this.createdButtonsBox[5]);
+      if (!this.listSelected.children.length) {
+        this.disableButton(this.createdButtonsBox[8]);
+      }
     }
   }
 
@@ -217,8 +374,20 @@ class ArrangeBoxContainer {
     for (let i = this.listSelected.children.length - 1; i >= 0; i--) {
       this.listAvailable.append(this.listSelected.children[i]);
     }
+
+    if (this.clickedListItem) {
+      this.clickedListItem.classList.remove('clicked_list_item');
+      this.clickedListItem = null;
+    }
+    this.undisableButton(this.createdButtonsBox[5]);
+    this.disableButton(
+      this.createdButtonsBox[6], this.createdButtonsBox[7], this.createdButtonsBox[8],
+      this.createdButtonsBox[9], this.createdButtonsBox[10],
+      this.createdButtonsBox[11], this.createdButtonsBox[12]
+    );
   }
 
+  
   // CONTROL AREA /////////////////////////////////
 
   createControlArea() {
@@ -247,6 +416,7 @@ class ArrangeBoxContainer {
   }
 
   resetToInitialState() {
+    this.clickedListItem = undefined;
     for (let i = this.listSelected.children.length - 1; i >= 0; i--) {
       this.listSelected.children[i].remove();
     }
@@ -257,8 +427,18 @@ class ArrangeBoxContainer {
     for (let value of this.initialAvailableValues) {
       let listElem = document.createElement('li');
       listElem.innerText = value;
+      listElem.addEventListener('click', () => this.clickListItem(listElem));
       this.listAvailable.append(listElem);
     }
+
+    for (let button of this.createdButtonsBox) {
+      this.disableButton(button);
+    }
+    this.undisableButton(
+      this.createdButtonsBox[4],   // add to Available
+      this.createdButtonsBox[5],   // move all to Selected
+      this.createdButtonsBox[13]   // add to Selected
+    );
   }
 
   showListItems(targetList) {
